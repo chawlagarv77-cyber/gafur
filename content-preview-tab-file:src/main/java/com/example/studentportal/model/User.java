990e60e95b1063c7ecee66959592c300e3e00ed4
chawlagarv77-cyber <chawlagarv77@gmail.com>
2025-10-06 00:08:56 +0530

    Create User.java

---
+++ content-preview-tab-file:src/main/java/com/example/studentportal/model/User.java
@@ -0,0 +1,73 @@
+package com.example.studentportal.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+  private String username;
+  private String password;
+  private List<String> roles;
+
+  public User() {}
+
+  public User(String username, String password, List<String> roles) {
+    this.username = username;
+    this.password = password;
+    this.roles = roles;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream().map(SimpleGrantedAuthority::new).toList();
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  public List<String> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<String> roles) {
+    this.roles = roles;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+}
